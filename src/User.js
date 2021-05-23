0c13b806c046a2077296fd6cd0544405b5c65cb6
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-05-23 14:46:18 +0600

    conditional rendaring done

--- src/User.js
+++ src/User.js
@@ -9,9 +9,19 @@ const User = (props) => {
     else{
         greetings = <h1>Do we familiar?</h1>
     }
+    
     return (
         <div>
+            <h2>Greetings</h2>
             <h2>{greetings}</h2>
+            <h2>food</h2>
+            {
+                familiar? <p>let's have some food my dear</p> : <p>ahare koydin dhore khan na apni?</p>
+            }
+            <h2>Contact</h2>
+            {
+                familiar && <h2>here is my visiting card abar dekha hobe ni hihi </h2> 
+            }
         </div>
     );
 };
